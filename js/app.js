d1967fcaa17a4cf6c78cfcf0c5525f5be3da6830
Cory Kinberger <coryk135@gmail.com>
2013-10-04 20:00:31 -0400

    edited gitignore to include bower_components

--- js/app.js
+++ js/app.js
@@ -10,10 +10,10 @@ var app = angular.module('ommfsched', [
 app.config(function($routeProvider){
     $routeProvider
     	
-    .when("/schedule", 	{controller:"ScheduleCtrl", 	templateUrl: "partials/schedule.html"})
-    .when("/map", 		{controller:"MapCtrl", 			templateUrl: "partials/map.html"})
-    .when("/info", 		{controller:"InfoCtrl", 		templateUrl: "partials/info.html"})
-    .when("/makers", 	{controller:"MakersCtrl", 		templateUrl: "partials/makers.html"})
+    .when("/schedule", 	{controller:"ScheduleCtrl", 	  templateUrl: "partials/schedule.html"})
+    .when("/map", 	   	{controller:"MapCtrl", 			    templateUrl: "partials/map.html"})
+    .when("/info", 		  {controller:"InfoCtrl", 		    templateUrl: "partials/info.html"})
+    .when("/makers",   	{controller:"MakersCtrl", 	   	templateUrl: "partials/makers.html"})
     .when("/makers/:id",{controller:"MakerDetailsCtrl", templateUrl: "partials/makerDetail.html"})
 
     .otherwise({redirectTo: "/schedule"})
